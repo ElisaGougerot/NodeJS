041aabfb92bb40a0119e050665e95562e500310d
Gougerot Elisa <elisagougerot@hotmail.fr>
2019-09-23 15:02:13 +0200

    exercice 3 ok

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
+
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
+})
+
+app.post('/chat', function (req, res) {
+
+  var message = req.body.msg
+
+  if (message === 'météo') {
+    res.send("Il fait beau \n")
+  } else if (message === 'ville') {
+    res.send("Vous êtes à Paris \n")
+  }
+});
+
+app.get('/hello', function (req,res) {
+  var nom = req.param('nom');
+
+  if (nom === undefined)  {
+    res.send('Quel est votre prénom ?')
+  }
+
+  else {
+    res.send('Bonjour, ' + nom)
+  }
+
 })
 
 app.listen(port, function () {
