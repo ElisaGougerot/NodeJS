71e6485f4a8bba7dfa13646f130f28321bd142da
Gougerot Elisa <elisagougerot@hotmail.fr>
2019-09-23 11:44:06 +0200

    port pour heroku

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var http = require("http");
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 8080;
 
-var server = http.createServer(function(req,res){
-  res.writeHead(200);
-  res.end('Hello World');
-});
-server.listen(8080);
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
